4a6368a8aaa28896f82bf166e376fe7c8d26bcdb
otabek-a <otabekabdurasulov245@gmail.com>
2025-06-18 14:58:21 +0500

    qwerty

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def post_comment():
         conn.commit()
 
     return jsonify({"status": "success"})
-
 if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0', port=5300, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
